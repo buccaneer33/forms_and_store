79fec983335f062df66b56cc1db6b06e3e707252
buccaneer33 <buccaneer33@inbox.ru>
2026-06-25 22:25:05 +0400

    [FINISH] dev branch added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './entities/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
